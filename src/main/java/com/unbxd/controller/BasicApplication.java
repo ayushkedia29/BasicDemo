65dde897aeea7b3c62c9132323ad0d7ac3d6db38
Ayush Bimal Kedia <ayush.bimal@unbxd.com>
2021-07-30 12:38:06 +0530

    Initial commit

---
+++ src/main/java/com/unbxd/controller/BasicApplication.java
@@ -0,0 +1,50 @@
+package com.unbxd.controller;
+
+import ro.pippo.core.Application;
+import ro.pippo.demo.common.Contact;
+import java.io.File;
+
+public class BasicApplication extends Application {
+
+    @Override
+    protected void onInit() {
+        // send 'Hello World' as response
+        GET("/", routeContext -> routeContext.send("Hello World"));
+
+        // send a file as response
+        GET("/file", routeContext -> routeContext.send(new File("pom.xml")));
+
+        // send a json as response
+        GET("/json", routeContext -> {
+            Contact contact = createContact();
+            routeContext.json().send(contact);
+        });
+
+        // send xml as response
+        GET("/xml", routeContext -> {
+            Contact contact = createContact();
+            routeContext.xml().send(contact);
+        });
+
+        // send an object and negotiate the Response content-type, default to XML
+        GET("/negotiate", routeContext -> {
+            Contact contact = createContact();
+            routeContext.xml().negotiateContentType().send(contact);
+        });
+
+        // send a template with name "hello" as response
+        GET("/template", routeContext -> {
+            routeContext.setLocal("greeting", "Hello");
+            routeContext.render("hello");
+        });
+    }
+
+    private Contact createContact() {
+        return new Contact()
+                .setId(12345)
+                .setName("John")
+                .setPhone("0733434435")
+                .setAddress("Sunflower Street, No. 6");
+    }
+
+}
